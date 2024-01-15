536f1dadce0ad167aaab5d779e730e20bf11f916
Carlos Eduardo Alves de Oliveira <devcarlosedeoliveira@outlook.com>
2024-01-15 14:49:58 -0300

    Clone

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 var box = document.getElementById("box");
 var pos = 0;
-var t = setInterval(move, 10);
+var t = setInterval(move, 1000);
 var acc = 0;
 
 function move(){
     while(acc < 10){
         if (pos >= 150){
-            acc += 1;
-            box.style.left = 0 + "px";
+            acc ++;
             pos = 0;
+            box.style.left = 0 + "px";
         } else {
-        pos += 1;
-        box.style.left = pos + "px";
+            pos ++;
+            box.style.left = pos + "px";
         }
     }
     clearInterval(t);
