a6386bf0890de975533d0bfc1a08b8aafc0a18bb
Carlos Eduardo Alves de Oliveira <devcarlosedeoliveira@outlook.com>
2024-01-15 17:33:22 -0300

    Novos Circulos
    Adição de novos círculos girando simultaneamente.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,109 @@
-var box = document.getElementById("box");
-var pos = 0;
-var t = setInterval(move, 1000);
-var acc = 0;
+var interval = 1
+var box0 = document.getElementById("box0");
+var box1 = document.getElementById("box1");
+var box2 = document.getElementById("box2");
+var box3 = document.getElementById("box3");
+var container = document.getElementById("container")
+var signals = [
+    {signal_right: false, signal_botton: false, signal_left: false, signal_top: false},
+    {signal_right: false, signal_botton: false, signal_left: false, signal_top: false},
+    {signal_right: false, signal_botton: false, signal_left: false, signal_top: false},
+    {signal_right: false, signal_botton: false, signal_left: false, signal_top: false}
+]
 
-function move(){
-    while(acc < 10){
-        if (pos >= 150){
-            acc ++;
-            pos = 0;
-            box.style.left = 0 + "px";
-        } else {
-            pos ++;
-            box.style.left = pos + "px";
-        }
+
+
+
+
+signals[0].signal_right = true;
+
+var box0_move = setInterval(() => {
+    movement(box0, container, signals[0]);
+}, interval);
+
+signals[1].signal_botton = true;
+
+var box1_move = setInterval(() => {
+    movement(box1, container, signals[1]);
+}, interval)
+
+signals[2].signal_left = true;
+
+var box1_move = setInterval(() => {
+    movement(box2, container, signals[2]);
+}, interval)
+
+signals[3].signal_top = true;
+
+var box1_move = setInterval(() => {
+    movement(box3, container, signals[3]);
+}, interval)
+
+
+
+
+
+
+
+
+function rightMove(el_move, el_limit, signal){
+    let pos = el_move.offsetLeft;
+    if (el_move.offsetLeft === el_limit.offsetWidth - el_move.offsetWidth){
+        signal.signal_right = false
+        return true;
+    } else{
+        pos++;
+        el_move.style.left = pos + "px"; 
+        return false;
     }
-    clearInterval(t);
+}
+
+function bottonMove(el_move, el_limit, signal){
+    let pos = el_move.offsetTop;
+    if (el_move.offsetTop === el_limit.offsetHeight - el_move.offsetHeight){
+        signal.signal_botton = false
+        return true;
+    } else{
+        pos++;
+        el_move.style.top = pos + "px"; 
+        return false;
+    }
+}
+
+function leftMove(el_move, signal){
+    let pos = el_move.offsetLeft;
+    if (el_move.offsetLeft === 0){
+        signal.signal_left = false
+        return true;
+    }else{
+        pos--;
+        el_move.style.left = pos + "px";
+        return false;
+    }
+}
+
+function topMove(el_move, signal){
+    let pos = el_move.offsetTop;
+    if (el_move.offsetTop === 0){
+        signal.signal_top = false
+        return true;
+    } else{
+        pos--;
+        el_move.style.top = pos + "px"; 
+        return false;
+    }
+}
+function movement(box, container, signal){
+    if (signal.signal_right){
+        signal.signal_botton = rightMove(box, container, signal);
+     }
+     if (signal.signal_botton){
+        signal.signal_left = bottonMove(box,container, signal);
+     }
+     if (signal.signal_left){
+        signal.signal_top = leftMove(box, signal);
+     }
+     if(signal.signal_top){
+        signal.signal_right = topMove(box, signal);
+     }
 }
